c32372b4df52d06d4ee8909033c5344c1f3642db
Jarred Allen <jarred@moveparallel.com>
2021-06-17 15:55:01 -0700

    Convert publish payload to string if it is a utf-8 string

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 use mqttrs::Packet;
+use std::convert::TryFrom;
+use std::string::FromUtf8Error;
+
+#[derive(Debug)]
+struct Publish {
+    dup: bool,
+    qospid: mqttrs::QosPid,
+    retain: bool,
+    topic_name: String,
+    payload: String
+}
+impl<'a> TryFrom<&'a mqttrs::Publish<'a>> for Publish {
+    type Error = FromUtf8Error;
+
+    fn try_from(publish: &'a mqttrs::Publish<'a>) -> Result<Self, FromUtf8Error> {
+        Ok(Self {
+            dup: publish.dup,
+            qospid: publish.qospid,
+            retain: publish.retain,
+            topic_name: publish.topic_name.to_owned(),
+            payload: String::from_utf8(publish.payload.into())?
+        })
+    }
+}
 
 pub fn format_packet(packet: Packet) -> String {
     match packet {
@@ -7,7 +31,13 @@ pub fn format_packet(packet: Packet) -> String {
         Packet::Subscribe(packet) => format!("{:#?}", packet),
         Packet::Unsubscribe(packet) => format!("{:#?}", packet),
         Packet::Suback(packet) => format!("{:#?}", packet),
-        Packet::Publish(packet) => format!("{:#?}\nPayload: {:?}", packet, String::from_utf8_lossy(packet.payload)),
+        Packet::Publish(packet) => {
+            if let Ok(packet) = Publish::try_from(&packet) {
+                format!("{:#?}", packet)
+            } else {
+                format!("{:#?}", packet)
+            }
+        },
         packet
         @
         (Packet::Puback(_)
